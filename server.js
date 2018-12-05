15b4a215b8b6e00020bb740ef2dcf537da7ecc66
Phụng Minh <1512414@student.hcmus.edu.vn>
2018-12-05 14:57:54 +0700

    aaa

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 // server.js
 var express = require('express');
+var cors = require('cors');
 var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
+app.use(cors());
 app.use(serveStatic(__dirname + "/dist"));
 var port = process.env.PORT || 3000;
 app.listen(port);
